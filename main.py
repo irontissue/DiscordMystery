dbc01ad332d0b9acfc618118ae380db973606e4f
Ashok Sankaran <ashok.sankaran1@xpo.com>
2019-03-27 17:52:46 -0700

    More random stuff, messing with accessing channel info. Bot now moves all users in Lobby to the Ballroom when !start_game is used.

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ logging.basicConfig(level=logging.INFO)
 
 TOKEN = 'NTYwNTQyMjY4MjUxOTYzNDAy.D31dcA.BF8HIB6fvbY2uizuBfJDTSExk50'
 
-client = discord.Client()
 bot = commands.Bot(command_prefix='!', description='Discord Mystery Bot Controller')
 
 
@@ -19,8 +18,20 @@ async def on_ready():
     print('------')
 
 
+# 560612701412392970
 @bot.command()
-async def echo(ctx, message):
-    await ctx.send(message)
+async def start_game(ctx):
+    channel_name = "Lobby"
+    lobby_id, lobby = get_channel_by_name(channel_name)
+    for member in lobby.members:
+        await ctx.send("Moving " + str(member) + " to Ballroom.")
+        await member.move_to(get_channel_by_name("Ballroom")[1])
+
+
+def get_channel_by_name(name):
+    for channel in bot.get_all_channels():
+        if channel.name == name:
+            return channel.id, channel
+
 
 bot.run(TOKEN)
